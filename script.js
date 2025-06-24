9a7f76937304708f755115f8c7e9092869de6854
Elias <elias.d.gonzalez25@gmail.com>
2025-06-24 00:33:28 -0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Hola mundo en Typescript");
+"use strict";
+/*console.log("Curso de Typescript");
+let estudiasteJavascript: boolean = true;
+
+if (estudiasteJavascript) {
+  console.log("podes ver el curso de Typescript");
+} else {
+  console.log("Debes ver antes el curso de Javascript");
+}
+*/
+let argentina = 11;
+let francia = 11;
+let messi = 1;
+let juegaMessi = true;
+function jugar(team1, team2, juegaMessi) {
+    let motivo = "";
+    if (juegaMessi) {
+        team1 += messi;
+        motivo = " porque juega Messi";
+    }
+    if (team1 > team2) {
+        console.log(`Gana Argentina${motivo}`);
+    }
+    if (team1 < team2) {
+        console.log("Gana Francia");
+    }
+    else {
+        console.log("Empatan");
+    }
+}
+jugar(argentina, francia, juegaMessi);
--- script.ts
+++ script.ts
@@ -1,1 +1,37 @@
-console.log("Hola mundo en Typescript");
+/*console.log("Curso de Typescript");
+let estudiasteJavascript: boolean = true;
+
+if (estudiasteJavascript) {
+  console.log("podes ver el curso de Typescript");
+} else {
+  console.log("Debes ver antes el curso de Javascript");
+}
+*/
+
+let argentina: number = 11;
+let francia: number = 11;
+let messi: number = 1;
+let juegaMessi: boolean = true;
+
+function jugar(team1: number, team2: number, juegaMessi: boolean): void {
+
+  let motivo: string = ""
+  
+  if (juegaMessi) {
+    team1 += messi;
+    motivo = " porque juega Messi"
+  }
+
+  if (team1 > team2) {
+    console.log(`Gana Argentina${motivo}`);
+  }
+  if (team1 < team2) {
+    console.log("Gana Francia");
+
+  } else {
+    console.log("Empatan");
+  }
+}
+
+jugar(argentina, francia, juegaMessi)
+
